43a2d5fc084f17ea54413e68ac496e4e027f372d
Andy Paul Chen <la.vache.qui.vit@gmail.com>
2024-10-10 15:49:15 +0800

    I launched Virp
    Make a pip installable package (test)

---
+++ virp/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2024, Andy Paul Chen
+# Distributed under the terms of the MIT License.
+
+"""
+Virp (VIRtual cell generation by Permutation) is a code for generating a virtual cell from a site-disordered .cif crystal file
+"""
+
+__copyright__  = "Andy Paul Chen"
+__version__    =  "0.1"
+__maintainer__ = "Andy Paul Chen"
+__email__      = "la.vache.qui.vit@gmail.com"
+__status__     = "Development"
+__date__       = "10 October 2024"
