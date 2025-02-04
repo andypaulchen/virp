be77a6da3845328c6add992505e38f71bdb1baf9
Andy Paul Chen <la.vache.qui.vit@gmail.com>
2025-02-04 14:51:44 +0800

    Update session folder

--- session/properties.py
+++ session/properties.py
@@ -5,8 +5,9 @@ for filename in os.listdir("_disordered_cifs"):
     # extract header string
     if filename.endswith(".cif"):  # Check if the file has a .cif extension
         header = os.path.splitext(os.path.basename(filename))[0]
-    # note: operation on structure-optimized cells only
-    VirtualCellProperties(os.path.join(header, "stropt"), os.path.join(header, "virtual_properties.csv"))
+        if os.path.exists(os.path.join(header, "_JOBDONE")):
+            # note: operation on structure-optimized cells only
+            VirtualCellProperties(os.path.join(header, "stropt"), os.path.join(header, "virtual_properties.csv"))
 
 # For expectation values post-processing step, run
 #ExpectationValues(csv_path, temperature)xanthom
