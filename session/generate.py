840738f25e10269f528c56542559416285ce0b37
Andy Paul Chen <la.vache.qui.vit@gmail.com>
2025-05-27 16:20:25 +0800

    new generate.py

--- session/generate.py
+++ session/generate.py
@@ -1,4 +1,4 @@
-from virp import Session,ML_Relaxer
+from virp import Session, ML_Relaxer
 import warnings
 import sys
 
@@ -9,16 +9,10 @@ with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
     # Initialize the ML_Relaxer
-    relaxer = ML_Relaxer(
-        calc_name="mace_omat",
-        calc_paths=None,
-        optimizer="LBFGSLineSearch",
-        device="cuda",
-        relax_cell=True,
-    )
+    mlrelaxer = ML_Relaxer(calc_name="mace_omat", calc_paths=None, optimizer="LBFGSLineSearch", relax_cell=True)
 
     # Default Settings
-    Session(folder_path = "_disordered_cifs", relaxer = relaxer)
+    Session(folder_path = "_disordered_cifs", relaxer=mlrelaxer)
 
     # Custom Settings (for testing)
-    #Session(folder_path = "_disordered_cifs", mindist = 10, sample_size = 2, relaxer = None)
+    #Session(folder_path = "_disordered_cifs", relaxer=mlrelaxer, mindist = 10, sample_size = 2)
