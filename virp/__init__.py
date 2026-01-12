9048bfc74f310b08eab2994e55b47f7ea6821e34
Andy Paul Chen <la.vache.qui.vit@gmail.com>
2026-01-12 14:39:51 +0800

    v1.4.5
    fix SessionICSD

--- virp/__init__.py
+++ virp/__init__.py
@@ -10,7 +10,7 @@ from .enumerate import *
 from .relax import *
 
 __copyright__  = "Kedar Hippalgaonkar's Materials by Design Lab"
-__version__    =  "1.4.4"
+__version__    =  "1.4.5"
 __maintainer__ = "Andy Paul Chen"
 __email__      = "la.vache.qui.vit@gmail.com"
 __status__     = "Development"
